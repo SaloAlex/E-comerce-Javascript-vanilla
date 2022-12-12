93c96086d46743f0fcdf3d061fa3ebc0365e61e1
Theflak0 <dvdsalomon6@gmail.com>
2022-12-12 15:03:39 -0300

    Primer subida del proyecto

---
+++ scripts.js/parasol.js
@@ -0,0 +1,26 @@
+const productos = [
+{
+    id: 4,
+    nombre: "Sol 1",
+    precio: 2500,
+    img:
+      "https://cdn.pixabay.com/photo/2020/08/14/06/11/glasses-5486996__340.jpg",
+    cantidad: 18,
+  },
+  {
+      id: 5,
+      nombre: "Sol 2",
+      precio: 2800,
+      img:
+        "https://cdn.pixabay.com/photo/2017/07/02/21/34/glasses-2465912__340.jpg",
+      cantidad: 15,
+    },
+    {
+      id: 6,
+      nombre: "Sol 3",
+      precio: 3200,
+      img:
+        "https://cdn.pixabay.com/photo/2017/12/20/00/08/gatorz-3028956__340.jpg",
+      cantidad: 10,
+    },
+];
